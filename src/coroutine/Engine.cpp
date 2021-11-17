2310400265fe83affd0eaf5c85483528865fac9a
Denis <den1s.rogozhin@mail.ru>
2021-11-17 23:56:15 +0300

    first

--- src/coroutine/Engine.cpp
+++ src/coroutine/Engine.cpp
@@ -7,13 +7,79 @@
 namespace Afina {
 namespace Coroutine {
 
-void Engine::Store(context &ctx) {}
 
-void Engine::Restore(context &ctx) {}
+Engine::~Engine() {
+	if (StackBottom) {
+		delete[] std::get<0>(idle_ctx->Stack);
+		delete idle_ctx;
+	}
+	while (alive) {
+		context *ctx = alive;
+		alive = alive->next;
+		delete[] std::get<0>(ctx->Stack);
+		delete ctx;
+	}
+	while (blocked) {
+		context *ctx = blocked;
+		blocked = blocked->next;
+		delete[] std::get<0>(ctx->Stack);
+		delete ctx;
+	}
+}
 
-void Engine::yield() {}
 
-void Engine::sched(void *routine_) {}
+void Engine::Store(context &ctx) {
+	char a;
+	ctx.Hight = &a;
+	std::size_t stack_size = ctx.Hight - ctx.Low;
+	if (std::get<1>(ctx.Stack) < stack_size) {
+		delete[] std::get<0>(ctx.Stack);
+		std::get<1>(ctx.Stack) = stack_size;
+		std::get<0>(ctx.Stack) = new char[stack_size];
+	}
+
+	memcpy(std::get<0>(ctx.Stack), ctx.Low, stack_size);
+		
+}
+
+void Engine::Restore(context &ctx) {
+	std::size_t stack_size = ctx.Hight - ctx.Low;
+	//
+	memcpy(ctx.Low, std::get<0>(ctx.Stack),stack_size);
+	cur_routine = &ctx;
+	longjmp(ctx.Environment, 1);
+}
+
+void Engine::yield() {
+	if (!alive || (cur_routine == alive && !alive->next)) {
+		return;
+	}
+	context *ctx = alive;
+	if (ctx == cur_routine) {
+		ctx = ctx->next;
+	}
+	if (ctx) {
+		sched(ctx);
+	}
+}
+
+void Engine::sched(void *routine_) {
+	context *routine = static_cast<context *>(routine_);
+	if (routine_ == nullptr) {
+		yield();
+		return;
+	}
+	if (cur_routine == routine || routine->is_blocked) {
+		return;
+	}
+	if (cur_routine != idle_ctx) {
+		if (setjmp(cur_routine->Environment) > 0) {
+			return;
+		}
+		Store(*cur_routine);
+	}
+	Restore(*routine);
+}
 
 } // namespace Coroutine
 } // namespace Afina
